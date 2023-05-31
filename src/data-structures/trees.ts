f0297cdc93e630f8a58647b3532a71a43f1afa01
ja153903 <jaimeabbariao@gmail.com>
2023-05-30 23:50:41 -0400

    feat: adding some tree related stuff

---
+++ src/data-structures/trees.ts
@@ -0,0 +1,15 @@
+export class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+
+  constructor(
+    val: number,
+    left: TreeNode | null = null,
+    right: TreeNode | null = null
+  ) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
---
+++ src/utils/trees.ts
@@ -0,0 +1,2 @@
+// TODO: Not sure what I'm going to need from this yet
+export class TreeHelper {}

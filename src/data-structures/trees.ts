347d20e52f4d0337c6a1e68737a6deda2e0e4996
ja153903 <jaimeabbariao@gmail.com>
2023-06-05 15:38:31 -0400

    feat: updating with some tree stuff

--- src/data-structures/trees.ts
+++ src/data-structures/trees.ts
@@ -13,3 +13,66 @@ export class TreeNode {
     this.right = right;
   }
 }
+
+export function inOrderTraversal(
+  root: TreeNode | null,
+  callback: (node: TreeNode) => unknown
+) {
+  if (root) {
+    inOrderTraversal(root.left, callback);
+    callback(root);
+    inOrderTraversal(root.right, callback);
+  }
+}
+
+export function preOrderTraversal(
+  root: TreeNode | null,
+  callback: (node: TreeNode) => unknown
+) {
+  if (root) {
+    callback(root);
+    preOrderTraversal(root.left, callback);
+    preOrderTraversal(root.right, callback);
+  }
+}
+
+export function postOrderTraversal(
+  root: TreeNode | null,
+  callback: (node: TreeNode) => unknown
+) {
+  if (root) {
+    postOrderTraversal(root.left, callback);
+    postOrderTraversal(root.right, callback);
+    callback(root);
+  }
+}
+
+export function levelOrderTraversal(
+  root: TreeNode | null,
+  callback: (node: TreeNode) => unknown
+) {
+  if (!root) {
+    return;
+  }
+
+  const queue: Array<TreeNode | null> = [];
+  queue.push(root);
+
+  while (queue.length > 0) {
+    const size = queue.length;
+
+    for (let i = 0; i < size; i++) {
+      const front = queue.pop()!;
+
+      callback(front);
+
+      if (front.left) {
+        queue.push(front.left);
+      }
+
+      if (front.right) {
+        queue.push(front.right);
+      }
+    }
+  }
+}
--- src/utils/trees.ts
+++ src/utils/trees.ts
@@ -1,2 +1,53 @@
-// TODO: Not sure what I'm going to need from this yet
-export class TreeHelper {}
+import {
+  TreeNode,
+  inOrderTraversal,
+  postOrderTraversal,
+  preOrderTraversal,
+} from "../data-structures/trees";
+
+export class TreeHelper {
+  static convertArrayIntoBalancedBinaryTree(
+    numbers: number[]
+  ): TreeNode | null {
+    if (numbers.length === 0) {
+      return null;
+    }
+
+    const root = new TreeNode(numbers.shift()!);
+
+    root.left = TreeHelper.convertArrayIntoBalancedBinaryTree(numbers);
+    root.right = TreeHelper.convertArrayIntoBalancedBinaryTree(numbers);
+
+    return root;
+  }
+
+  static toInOrderArray(root: TreeNode | null): number[] {
+    const result: number[] = [];
+
+    inOrderTraversal(root, (node) => {
+      result.push(node.val);
+    });
+
+    return result;
+  }
+
+  static toPreOrderArray(root: TreeNode | null): number[] {
+    const result: number[] = [];
+
+    preOrderTraversal(root, (node) => {
+      result.push(node.val);
+    });
+
+    return result;
+  }
+
+  static toPostOrderArray(root: TreeNode | null): number[] {
+    const result: number[] = [];
+
+    postOrderTraversal(root, (node) => {
+      result.push(node.val);
+    });
+
+    return result;
+  }
+}

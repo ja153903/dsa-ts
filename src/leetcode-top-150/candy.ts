6e4241c651926b3db789ffebf59a65bb86480dab
ja153903 <jaimeabbariao@gmail.com>
2023-06-10 15:07:39 -0400

    feat: updating with comments

--- src/leetcode-top-150/candy.ts
+++ src/leetcode-top-150/candy.ts
@@ -1,12 +1,15 @@
 function candy(ratings: number[]): number {
   const candyCount = new Array(ratings.length).fill(1);
 
+  // update counts if value is greater than the previous
   for (let i = 1; i < ratings.length; i++) {
     if (ratings[i] > ratings[i - 1]) {
       candyCount[i] = candyCount[i - 1] + 1;
     }
   }
 
+  // This loop makes sure that the higher rating gets more candy
+  // than its right neighbor
   for (let i = ratings.length - 1; i > 0; i--) {
     if (ratings[i - 1] > ratings[i]) {
       candyCount[i - 1] = Math.max(candyCount[i] + 1, candyCount[i - 1]);

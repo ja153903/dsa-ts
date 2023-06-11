33059832d89e3bb2727cd5a0cffb5cfe324985f1
ja153903 <jaimeabbariao@gmail.com>
2023-06-10 23:50:05 -0400

    feat: adding trapping rain water
    But we need to learn how to properly explain the solution to this
    problem

---
+++ src/leetcode-top-150/trapping-rain-water.ts
@@ -0,0 +1,31 @@
+function trap(height: number[]): number {
+  let left = 0,
+    right = height.length - 1;
+  let maxLeft = height[left],
+    maxRight = height[right];
+  let result = 0;
+
+  while (left < right) {
+    if (height[left] < height[right]) {
+      if (height[left] > maxLeft) {
+        maxLeft = height[left];
+      } else {
+        result += maxLeft - height[left];
+      }
+
+      left++;
+    } else {
+      if (height[right] > maxRight) {
+        maxRight = height[right];
+      } else {
+        result += maxRight - height[right];
+      }
+
+      right--;
+    }
+  }
+
+  return result;
+}
+
+export { trap };

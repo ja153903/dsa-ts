104642fb80990ce4e6afd89170f4d8242a3518c5
ja153903 <jaimeabbariao@gmail.com>
2023-06-12 14:49:08 -0400

    feat: updating tree solution

--- src/data-structures/trees.ts
+++ src/data-structures/trees.ts
@@ -1,17 +1,9 @@
 export class TreeNode {
-  val: number;
-  left: TreeNode | null;
-  right: TreeNode | null;
-
   constructor(
-    val: number,
-    left: TreeNode | null = null,
-    right: TreeNode | null = null
-  ) {
-    this.val = val;
-    this.left = left;
-    this.right = right;
-  }
+    public val: number,
+    public left: TreeNode | null = null,
+    public right: TreeNode | null = null
+  ) {}
 }
 
 export function inOrderTraversal(

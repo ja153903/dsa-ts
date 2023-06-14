6c1c2b3ef1b36dff28451c0d0b6929178d5977a0
ja153903 <jaimeabbariao@gmail.com>
2023-06-14 00:25:54 -0400

    feat: adding solution

---
+++ src/daily-problems/minimum-absolute-difference-in-bst.ts
@@ -0,0 +1,32 @@
+import { TreeNode } from "../data-structures/trees";
+
+function getMinimumDifference(root: TreeNode | null): number {
+  // if we have a BST the minimum difference can be found by just looking at
+  // the inorder traversal
+  const stack = [];
+  let prev = null;
+  let result = Infinity;
+
+  while (stack.length || root) {
+    while (root) {
+      stack.push(root);
+      root = root.left;
+    }
+
+    root = stack.pop() ?? null;
+
+    if (root) {
+      if (prev !== null) {
+        result = Math.min(result, Math.abs(prev - root.val));
+      }
+
+      prev = root.val;
+
+      root = root.right;
+    }
+  }
+
+  return result;
+}
+
+export { getMinimumDifference };

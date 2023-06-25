130eedbfee8eef8ed2b2b97f54ff20a8071c0852
ja153903 <jaimeabbariao@gmail.com>
2023-06-25 15:32:10 -0400

    feat: updating

--- src/advent-of-code/aoc2015/day8.ts
+++ src/advent-of-code/aoc2015/day8.ts
@@ -1,3 +1,15 @@
 import { readFile, createFilePathToData } from "../utils";
 
 export const content = readFile(createFilePathToData(2015, 8));
+
+function parseData(data: string): string[] {
+  return data.split("\n");
+}
+
+export function part1(data: string): number {
+  const strings = parseData(data);
+
+  return strings.reduce((acc, s) => {
+    return acc;
+  }, 0);
+}

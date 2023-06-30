a60a0939e7396358d075b3f1ed1fe9b80ec10488
ja153903 <jaimeabbariao@gmail.com>
2023-06-30 17:01:08 -0400

    feat: updating with todo

--- src/advent-of-code/aoc2015/day11.ts
+++ src/advent-of-code/aoc2015/day11.ts
@@ -1,5 +1,11 @@
+// TODO: Solve this problem later
+
 export const content = "hepxcrrq";
 
-export function part1(data: string): string {}
+export function part1(data: string): string {
+  return "";
+}
 
-export function part2(data: string): string {}
+export function part2(data: string): string {
+  return "";
+}
--- src/advent-of-code/aoc2015/tests/day11.test.ts
+++ src/advent-of-code/aoc2015/tests/day11.test.ts
@@ -1,7 +1,8 @@
 import { content, part1 } from "../day11";
+
 describe("AOC 2015 - Day 11", () => {
   describe("Part 1", () => {
-    it("should pass some cases", () => {
+    xit("should pass some cases", () => {
       expect(part1(content)).toBe("");
     });
   });
